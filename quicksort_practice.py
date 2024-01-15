9cb6e11569995e34e5b94ba5f15dcfe9b11fd9ab
Vityuk Yuriy <you.thunderstruck@gmail.com>
2024-01-15 08:33:33 +0200

    add quicksort

---
+++ quicksort_practice.py
@@ -0,0 +1,12 @@
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    right = [x for x in arr if x > pivot]
+    middle = [x for x in arr if x == pivot]
+
+    return quicksort(left) + middle + quicksort(right)
+
+
+print(quicksort([7, 12, 3, 5, 8, 11, 20, 1, 6, 14]))
